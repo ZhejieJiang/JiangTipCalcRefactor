b1a3511619d1fc2cab60a231ea47e2489af0cf92
Zhejie Jiang <zhejiej@nycstudents.net>
2022-10-07 09:28:38 -0400

    working on this

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -1,2 +1,43 @@
-package PACKAGE_NAME;public class TipCalculator {
+import java.lang.Math;
+public class TipCalculator {
+    private int numPeople;
+    private int tipPercentage;
+    private double totalBillBeforeTip;
+
+    public TipCalculator(int people, int tip){
+        tipPercentage = tip;
+        numPeople = people;
+        totalBillBeforeTip = 0.0;
+    }
+    public double getTotalBillBeforeTip(){
+        return totalBillBeforeTip;
+    }  //getter method that returns the totalBillBeforeTip instance variable
+
+    public int getTipPercentage() {
+        return tipPercentage;
+    }  //getter method that returns the tipPercentage instance variable
+
+    public void addMeal(double cost){
+        totalBillBeforeTip += cost;
+    } //method that increments totalBillBeforeTip by cost and returns no value
+
+    public double tipAmount(){
+        return Math.round((totalBillBeforeTip * tipPercentage)*100)/100;
+    } //method that calculates and returns a double representing the total tip amount added to the bill
+
+    public double totalBill(){
+        return totalBillBeforeTip + tipAmount();
+    } //method that calculates and returns a double representing the group’s total bill including tip
+
+    public double perPersonCostBeforeTip(){
+        return Math.round((totalBillBeforeTip/numPeople)*100)/100;
+    } //method that calculates and returns a double representing the per person cost before tip
+
+    public double perPersonTipAmount(){
+        return Math.round((tipAmount()/numPeople)*100)/100;
+    } //method that calculates and returns a double representing the per person tip amount
+
+    public double perPersonTotalCost(){
+        return Math.round((totalBill()/numPeople)*100)/100;
+    } //method that calculates and returns a double representing the total cost per person
 }
--- src/TipCalculatorRunner.java
+++ src/TipCalculatorRunner.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class TipCalculatorRunner {
+//Imports
+import java.util.Scanner; //For Scanners
+public class TipCalculatorRunner
+{
+    public static void main(String[]args)
+    {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("How many people are in your group? ");
+        int a = scan.nextInt();
+        System.out.print("What's the tip percentage? (0-100): ");
+        int b = scan.nextInt();
+        System.out.print("What is your bill? ");
+        double c = scan.nextDouble();
+
+        TipCalculator main = new TipCalculator(a,b);
+        main.addMeal(c);
+
+
+        //Writing out the answers
+        System.out.println();
+        System.out.println("The total bill including Tip amount is $" + main.totalBill());
+        System.out.println("The total tip amount is $" + main.tipAmount());
+        System.out.println("The total bill including Tip per person is $" + main.perPersonTotalCost());
+        System.out.println("The total tip per person is $" + main.perPersonTipAmount());
+
+    }
 }
+/*import java.text.DecimalFormat;
+        DecimalFormat df=new DecimalFormat("0.00");
+        String formate = df.format(value);
+        double finalValue = (Double)df.parse(formate); */
